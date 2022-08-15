b32b5c64de82ec238ae60cba42fbe7ee16e508f6
Pavel <web2cap@gmail.com>
2022-08-15 02:57:15 +0200

    add fixtures

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -58,25 +58,20 @@ TEMPLATES = [
 WSGI_APPLICATION = "api_yamdb.wsgi.application"
 
 
-#DATABASES = {
-#    "default": {
-#        "ENGINE": os.getenv(
-#            "DB_ENGINE",
-#            default="django.db.backends.postgresql",
-#        ),
-#        "NAME": os.getenv("DB_NAME", default="postgres"),
-#        "USER": os.getenv("POSTGRES_USER", default="postgres"),
-#        "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
-#        "HOST": os.getenv("DB_HOST", default="db"),
-#        "PORT": os.getenv("DB_PORT", default="5432"),
-#    }
-#}
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": os.getenv(
+            "DB_ENGINE",
+            default="django.db.backends.postgresql",
+        ),
+        "NAME": os.getenv("DB_NAME", default="postgres"),
+        "USER": os.getenv("POSTGRES_USER", default="postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
+        "HOST": os.getenv("DB_HOST", default="db"),
+        "PORT": os.getenv("DB_PORT", default="5432"),
     }
 }
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
